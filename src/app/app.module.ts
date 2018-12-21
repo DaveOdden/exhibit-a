a7dfa5c5487fc3ea2f645b00176026ac81503030
daveodden <davidpodden@gmail.com>
2018-12-20 21:19:08 -0500

    rewrite secrets

---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+// Boilerplate
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+// Third-Party
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider,
+} from "angular-6-social-login";
+
+// Custom
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
+import { StatusComponent } from './status/status.component';
+import { AppHeaderDirective } from './__elements/app-header/app-header.directive';
+
+import * as secrets from '../../credentials.json';
+
+// Configs 
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+    [ {
+      id: GoogleLoginProvider.PROVIDER_ID,
+      provider: new GoogleLoginProvider(secrets.default.web.client_id)
+    } ]
+  );
+  return config;
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    StatusComponent,
+    AppHeaderDirective
+  ],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    SocialLoginModule,
+    AppRoutingModule,
+  ],
+  providers: [{
+    provide: AuthServiceConfig,
+    useFactory: getAuthServiceConfigs
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+  constructor() {
+    console.log(secrets.default.web.client_id);
+  }
+}
