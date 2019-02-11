702ea63f01bba66041a724618dd4525782ccd1bb
daveodden <davidpodden@gmail.com>
2019-02-10 23:39:25 -0500

    remove app component router intercepter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,28 +20,7 @@ import '@vaadin/vaadin-list-box/vaadin-list-box.js';
 
 export class AppComponent {
 
-	// authIsLoading: boolean = true;
-	// userIsLoggedIn: boolean = false;
-  // user;
-  
-  constructor( private socialAuthService: AuthService,
-               private router: Router,
-               private store: Store<AppState> ) {
-    // console.log('in app component');
-		// this.socialAuthService.authState.subscribe((user) => {
-    //   this.user = user;
-		// 	this.userIsLoggedIn = (user != null);
-		// 	this.authIsLoading = user !== null ? false : true;
-    // });
-
-    router.events.subscribe((val) => {
-      // console.log('router event');
-      //console.log(router); 
-      if(val instanceof NavigationEnd) {
-        //this.store.dispatch(new StateActions.ChangeHeaderTitle('Fart Knocker') );
-        //this.store.dispatch(new StateActions.ChangeHeaderAttributes(<any>val) );
-      }
-    });
+  constructor( private socialAuthService: AuthService, private router: Router, private store: Store<AppState> ) {
   }
 
   prepareRoute(outlet: any) {
