ad45ea902f8a8f28a69ddabb1bde78864e46d9cc
Dave Odden <davidpodden@gmail.com>
2019-02-19 23:47:55 -0500

    cleaned up app status page with auth status and heroku connection status

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService, GoogleLoginProvider } from 'angular-6-social-login';
 import { Store } from '@ngrx/store';
-import { NgRxStore, AppState } from '../app.interfaces';
+import { NgRxStore, AppState, selectAuthState } from '../app.interfaces';
 import * as StateActions from '../__state/state.actions';
 import { HerokuApiService } from '../__services/heroku.api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-status',
+	styleUrls: ['./status.component.scss'],
   templateUrl: './status.component.html'
 })
 export class StatusComponent implements OnInit {
 
   authIsRetrieved: boolean = false;
 	viewIsLoading: boolean = true;
-	authIsLoading: boolean = true;
-	userIsLoggedIn: boolean = false;
-  user;
+  authIsLoading: boolean = true;
+  
 
+	userIsLoggedIn: boolean = false;
+  username: string;
+  user;
   isConnectedToHeroku = false;
   
   constructor( private socialAuthService: AuthService, private store: Store<AppState>, private herokuApi: HerokuApiService ) {
@@ -24,6 +28,10 @@ export class StatusComponent implements OnInit {
     //this.store.dispatch( new StateActions.RestoreSession()
     this.getState();
     this.setHeaderAttributes();
+
+    this.store.select('appState').subscribe((state) => {
+      console.log(state);
+    });
   }
 
   ngOnInit() {
